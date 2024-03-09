6e717a63c80e40879650595deefd71187f17068e
MOHSEN <mohsen.mhm23@gmail.com>
2024-03-09 20:56:17 +0330

    Add barriers and refactor code

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 '''
-River Raid Game
+Cat and Mouse Game
 ------------------------------------
 '''
 import random
@@ -9,16 +9,24 @@ import time
 player = {
     "l": 1,
     "c": 1,
-    "char": "🐱", # 🐱
+    "char": "🐱",
     "alive": False
 }
 
 enemy = {
     "l": 1,
     "c": 1,
-    "char": "🐭", # 🐭
+    "char": "🐭",
     "alive": False
 }
+
+barrier = {
+    "char": "🚧",
+    "drawed": False,
+    "count": 20,
+    "positions": set()
+}
+
 
 player_score = 0
 
@@ -31,6 +39,19 @@ def draw_enemy(stdscr):
         stdscr.addstr(enemy.get("l"), enemy.get("c"), enemy.get('char'))
     else:
         stdscr.addstr(enemy.get("l"), enemy.get("c"), enemy.get('char'))
+
+def draw_barrier(stdscr):
+    if not barrier.get('drawed'):
+        for _ in range(barrier.get('count')):
+            barrier_c = random.randint(1, curses.COLS - 5)
+            barrier_l = random.randint(1, curses.LINES - 5)
+            if (barrier_l, barrier_c) not in barrier["positions"] and (barrier_l, barrier_c) != (player.get('l'), player.get('c')) and (barrier_l, barrier_c) != (enemy.get('l'), enemy.get('c')):
+                stdscr.addstr(barrier_l, barrier_c, barrier.get('char'))
+                barrier["positions"].add((barrier_l, barrier_c))
+        barrier.update({'drawed': True})
+    else:
+        for loc in barrier["positions"]:
+            stdscr.addstr(loc[0], loc[1], barrier.get('char'))
 
 
 def draw_world(stdscr):
@@ -52,6 +73,7 @@ def draw_world(stdscr):
             add_score_text(stdscr)
             draw_enemy(stdscr)
             draw_player(stdscr)
+            draw_barrier(stdscr)
 
             # Refresh the screen
             stdscr.refresh()
@@ -64,31 +86,40 @@ def draw_world(stdscr):
             update_player_score()
 
         else:
-            print('You die!')
+            stdscr.addstr(0, 0, 'You die!')
+            stdscr.refresh()
             time.sleep(1)
-            exit()
-
+            break
 
 def update_player_score():
     global player_score
     if ((player.get('c') == enemy.get('c')) or (player.get('c') == enemy.get('c') - 1) or (player.get('c') == enemy.get('c') + 1)) and (player.get('l') == enemy.get('l')):
         enemy.update({'alive': False})
         player_score += 1
+        curses.beep()
 
 # Check input and move player
 def move_player(key):
-        if key == ord('w'):
-            if player.get('l') != 1:
-                player.update({"l": player.get('l') - 1})
-        elif key == ord('s'):
-            if player.get('l') != curses.LINES - 2:
-                player.update({'l': player.get('l') + 1})
-        elif key == ord('a'):
-            if player.get('c') != 1:
-                player.update({'c': player.get('c') - 1})
-        elif key == ord('d'):
-            if player.get('c') != curses.COLS - 3:
-                player.update({'c': player.get('c') + 1})
+    new_l, new_c = player.get('l'), player.get('c')
+    if key == ord('w'):
+        new_l -= 1
+    elif key == ord('s'):
+        new_l += 1
+    elif key == ord('a'):
+        new_c -= 1
+    elif key == ord('d'):
+        new_c += 1
+
+    # Check if the new position is within a 1-cell radius of a barrier
+    too_close_to_barrier = False
+    for loc in barrier["positions"]:
+        if abs(loc[0] - new_l) < 1 and abs(loc[1] - new_c) <= 1:
+            too_close_to_barrier = True
+            break
+
+    # Update the player's position if not too close to a barrier
+    if not too_close_to_barrier:
+        player.update({"l": new_l, "c": new_c})
 
 def add_score_text(stdscr):
     score_text = f"Score: {player_score}"
